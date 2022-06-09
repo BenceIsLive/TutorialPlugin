e52d60b4d486fc3f7865912a3be0b80337b6a48a
benceislive <benceislive@gmail.com>
2022-06-09 20:31:58 +0200

    Added some new things to JoinEvent, new PlayerInteractEvent, made a ReloadCommand, JoinMessageCommand and added a lot to config.yml

--- src/main/java/dev/bence/tutorial/Tutorial.java
+++ src/main/java/dev/bence/tutorial/Tutorial.java
@@ -6,7 +6,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tutorial extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
